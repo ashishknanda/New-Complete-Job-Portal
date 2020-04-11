b6d8a443a22180f67b728b71f2dd002de4e3edd5
Ashish Kumar <33954666+ashishknanda@users.noreply.github.com>
2020-04-11 12:54:35 +0530

    Add files via upload

---
+++ dbconfig/config.php
@@ -0,0 +1,8 @@
+<?php
+/*For My LocalPC*/
+$con=mysqli_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db ($con,'job');
+?>
+
+
+
